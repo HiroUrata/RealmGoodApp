cef0a31ec03bdf6a0b79d34019c5683858b838f4
HiroUrata <1712026@i.softbank.jp>
2021-09-21 18:37:20 +0900

    UISearchController 追加

--- RealmGoodApp/ Controller/ViewController.swift
+++ RealmGoodApp/ Controller/ViewController.swift
@@ -10,34 +10,52 @@ import UIKit
 class ViewController: UIViewController{
 
     @IBOutlet weak var tableView: UITableView!
-    let realmCRUDModel = RealmCRUDModel()
     
-    let cellContentsArray = ["1","2","3","4","5","6","7","8","9","10"]
+    let searchController = UISearchController()
+    
+    let cellContentsArray = ["UITableView","UISearchController","UIView","UIColor","UICollectionView","UISwich","UIKit","UITextField","UIViewController","UILabel"]
+    
+    var searchResultArray = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.searchController = searchController
+        
         tableView.delegate = self
         tableView.dataSource = self
-        
-    }
-
-    @IBAction func goList(_ sender: Any) {
-        
-        let GoodListVC = storyboard?.instantiateViewController(identifier: "GoodListVC") as! GoodListViewController
-        
-        navigationController?.pushViewController(GoodListVC, animated: true)
-        
+        searchController.searchBar.delegate = self
     }
     
-
+   
 }
+
+extension ViewController:UISearchBarDelegate{
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        searchResultArray = []
+        cellContentsArray.forEach { cellContent in
+            
+            if cellContent.contains(searchText) == true {
+                
+                searchResultArray.append(cellContent)
+                tableView.reloadData()
+            }else if searchText == ""{
+                
+                tableView.reloadData()
+            }
+        }
+    }
+    
+}
+
 
 extension ViewController:UITableViewDelegate,UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         
-        return 77
+        return tableView.frame.size.height / 7
         
     }
     
@@ -49,45 +67,33 @@ extension ViewController:UITableViewDelegate,UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return cellContentsArray.count
-        
+        return {() -> Int in
+            
+            switch searchResultArray.count > 0{
+            
+            case true: return searchResultArray.count
+                
+            case false: return cellContentsArray.count
+                
+            }
+        }()
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
-        let cellContentsLabel = cell.contentView.viewWithTag(1) as! UILabel
-        let cellHeartButton = cell.contentView.viewWithTag(2) as! UIButton
-        
-        cellContentsLabel.text = cellContentsArray[indexPath.row]
-        cellHeartButton.tag = indexPath.row
-        cellHeartButton.addTarget(self, action: #selector(goodOrNoGood), for: .touchDown)
+        cell.textLabel?.text = {() -> String in
+           
+            switch searchResultArray.count > 0{
+            
+            case true: return searchResultArray[indexPath.row]
+                
+            case false: return cellContentsArray[indexPath.row]
+            }
+        }()
         
         return cell
     }
-     
-
-    @objc func goodOrNoGood(sender:UIButton){
-        
-        if sender.backgroundImage(for: .normal) == UIImage(systemName: "heart"){
-            
-            realmCRUDModel.createGoodRealm(goodLabel: cellContentsArray[sender.tag])
-            
-            sender.setBackgroundImage(UIImage(systemName: "heart.fill"), for: .normal)
-            print(sender.tag)
-            
-        }else{
-            
-            realmCRUDModel.deleteRealm(selectContents: cellContentsArray[sender.tag])
-            sender.setBackgroundImage(UIImage(systemName: "heart"), for: .normal)
-            print(sender.tag)
-        }
-        
-    }
-    
-    
-    
-    
     
 }
